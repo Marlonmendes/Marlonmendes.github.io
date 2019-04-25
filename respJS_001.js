6589aab04184b0b379bcece32c8740707c6294d3
aluno <aluno@lab3q105-p5qc01.ufu.br>
2019-04-25 11:26:46 -0300

    resp

---
+++ respJS_001.js
@@ -0,0 +1,17 @@
+function Q1() {
+    console.log('Q1');
+    var x = Number.parseInt(document.getElementById('x').value);
+    var y = Number.parseInt(document.getElementById('y').value);
+    // Segue aqui seu código
+    console.log('X / Y: ' + x + ' / ' + y);
+    console.log('Quociente = ' + Math.floor(x / y));
+    console.log('Resto = ' + (x % y));
+}
+
+
+function Q2() {
+    console.log('Q2');
+    var c1 = Number.parseFloat(document.getElementById('c1').value);
+    var c2 = Number.parseFloat(document.getElementById('c2').value);
+    // Segue aqui seu código
+}
